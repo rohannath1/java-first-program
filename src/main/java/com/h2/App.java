4cea72094e5df126618fc5bd756efd3150332bac
Rohan Nath <rohannath1510@gmail.com>
2021-11-20 11:55:14 +0530

    1st commit on this project

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -12,6 +12,15 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return 2*number;
     }
+
+    private static int add(int[] numbers){
+        int sum = 0;
+        for(int i; i<numbers.length; i++){
+            sum += numbers[i];
+        }
+        return sum;
+    }
+
 }
